c49f113053cd16e85b791284b96e92db734f7458
anugrahSA <anugrahnugi321@gmail.com>
2022-03-24 15:21:12 +0800

    menambahkan class SayaTubeVideo

--- tpmodul5_1302204047/Program.cs
+++ tpmodul5_1302204047/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul5_1302204047
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo x = new SayaTubeVideo("Tutorial Design By Contract - [Anugrah Saputra Aras]");
+            
+        }
+    }
+}
---
+++ tpmodul5_1302204047/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302204047
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            Random random = new Random();
+            id = random.Next(0, 99999);
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int PlayCount)
+        {
+            playCount++;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID VIDEO : " + id);
+            Console.WriteLine("JUDUL : " + title);
+            Console.WriteLine("JUMLAHDIMAINKAN : " + playCount);
+        }
+    }
+}
